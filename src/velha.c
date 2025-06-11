931b41ce9f8e214399a40cd1ae7e1d41e7e35ccd
Marcelo Marques Rodrigues <marques.marcelo.rodrigues@gmail.com>
2025-06-11 19:25:34 -0300

    Passado Primeiro Teste

--- src/velha.c
+++ src/velha.c
@@ -1,5 +1,23 @@
 #include "velha.h"
+#include <stdbool.h> // Usaremos para a flag
 
 int verificaVelha(int tabuleiro[3][3]) {
-    return 99; //  Falha do teste
+    bool temPeca = false;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (tabuleiro[i][j] != 0) {
+                temPeca = true;
+                break; // Achar peça
+            }
+        }
+        if (temPeca) {
+            break; // Se já achamos uma peça, não precisa procurar mais no tabuleiro
+        }
+    }
+
+    if (!temPeca) {
+        return -1; // Sem peça retorna indefinido
+    }
+
+    return 99;
 }
