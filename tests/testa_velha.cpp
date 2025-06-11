15307c7025e23eb31d1f358b9cadfe4185647f85
Marcelo Marques Rodrigues <marques.marcelo.rodrigues@gmail.com>
2025-06-11 20:03:14 -0300

    Falha Teste de Vitória em Coluna de O, Falaha Teste de Empate

--- tests/testa_velha.cpp
+++ tests/testa_velha.cpp
@@ -16,6 +16,17 @@ TEST_CASE("1: Jogo Indefinido - Tabuleiro Vazio", "[indefinido]") {
     //  Jogo indefinido deve retornar -1
     REQUIRE(verificaVelha(tabuleiro) == -1);}
 
+TEST_CASE("2: Jogo Empatado", "[empate]") {
+    // Tabuleiro cheio, sem vencedor.
+    int tabuleiro[3][3] = {
+        {1, 2, 1},
+        {1, 1, 2},
+        {2, 1, 2}
+    };
+    // Um jogo empatado deve retornar 0 
+    REQUIRE(verificaVelha(tabuleiro) == 0);
+}
+
 TEST_CASE("3: Vitoria do X - Linha Horizontal", "[vitoria-x]") {
     int tabuleiro[3][3] = {
         {1, 1, 1},  // X Ganha
@@ -24,3 +35,15 @@ TEST_CASE("3: Vitoria do X - Linha Horizontal", "[vitoria-x]") {
     };
     // A função deve retornar 1 se o X for o vencedor
     REQUIRE(verificaVelha(tabuleiro) == 1);}
+
+TEST_CASE("4: Vitoria do O - Coluna Vertical", "[vitoria-o]") {
+    // O (2) vence na segunda coluna
+    int tabuleiro[3][3] = {
+        {1, 2, 1},
+        {0, 2, 0},
+        {1, 2, 0}
+    };
+
+    // A função deve retornar 2 se o O for o vencedor
+    REQUIRE(verificaVelha(tabuleiro) == 2);
+}
